0b774b860bb3163cd916326d3fa6076584198269
kosuke978 <heitiankangjie487@gmail.com>
2025-12-19 23:33:31 +0900

    add cors設定

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from db.database import engine, Base
 
 from models.user import User
@@ -12,6 +13,22 @@ from services.ml_score_service import load_model  # ★追加
 
 app = FastAPI()
 
+# CORS設定
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:8080",
+    "http://127.0.0.1:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # モデルを読み込んだあとにテーブル作成
 Base.metadata.create_all(bind=engine)
 
@@ -20,9 +37,9 @@ app.include_router(auth.router)
 app.include_router(tasks.router)
 app.include_router(event_logs.router)
 app.include_router(ai.router)
+app.include_router(plants.router)
 
 # 起動時に1回だけMLモデルをロード
 @app.on_event("startup")
 def _startup():
     load_model()
-app.include_router(plants.router)
